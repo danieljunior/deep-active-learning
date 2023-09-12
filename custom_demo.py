a445bd92e3514ed3740a739d8049212789bd9125
Daniel Junior <danieljunior@id.uff.br>
2023-09-12 11:04:16 -0300

    Adding mechanism to clear cache and reduce memory usage

--- custom_demo.py
+++ custom_demo.py
@@ -8,6 +8,13 @@ from utils import get_dataset, get_net, get_strategy, get_params
 from custom_nets import Net as CustomNet, BertForNSP, SBERTCrossEncoderFinetune
 from custom_data import get_STS_data
 
+class ClearCache:
+    def __enter__(self):
+        torch.cuda.empty_cache()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        torch.cuda.empty_cache()
+
 seed = 42
 # fix random seed
 np.random.seed(seed)
@@ -52,8 +59,8 @@ nsp_base_models = {
     }
 train_params = {'n_epochs': 1,
                 # 'train_batch_size': 4,
-                # 'train_batch_size': 8,
-                'train_batch_size': 16
+                'train_batch_size': 8,
+                # 'train_batch_size': 16
                 }
 
 strategies = [
@@ -77,144 +84,147 @@ for dataset_name in datasets:
             ################################BASELINE#######################
             print("============================>BASELINE<=============================\n")
             for model_name, model_path in sbert_base_models.items():
+                with ClearCache():
+                    dataset = get_STS_data(dataset_name, sample, seed)  # load dataset
+                    net = SBERTCrossEncoderFinetune(model_path, device)  # load network
+                    test_data = np.fromiter(map(lambda x: x.label, dataset.get_test_data()), dtype=int)
 
-                dataset = get_STS_data(dataset_name, sample, seed)  # load dataset
-                net = SBERTCrossEncoderFinetune(model_path, device)  # load network
-                test_data = np.fromiter(map(lambda x: x.label, dataset.get_test_data()), dtype=int)
-
-                config = {"train_config": train_params,
-                          "model": model_name,
-                          "test_data": test_data,
-                          "seed": seed,
-                          "samples": sample,
-                          "strategy": "RAW BASELINE",
-                          "dataset": dataset_name.upper()}
-
-                run = wandb.init(project="Legal DeepAL", reinit=True, config=config, tags=[version])
-                wandb.run.name = model_name + ' -> RAW BASELINE'
-
-                preds = net.predict(dataset.get_test_data()).cpu()
-                accuracy = dataset.cal_test_acc(preds)
-                print(f"Raw Baseline testing accuracy: {accuracy}")
-
-                wandb.log({
-                    "total_labeled_data": 0,
-                    'predictions': preds.numpy(),
-                    'test_accuracy': accuracy})
-                run.finish()
-                print("================================================================\n")
-
-                if model_name != 'SBERT_STJ_IRIS':
                     config = {"train_config": train_params,
                               "model": model_name,
                               "test_data": test_data,
                               "seed": seed,
                               "samples": sample,
-                              "strategy": "ALL TRAIN DATA",
+                              "strategy": "RAW BASELINE",
                               "dataset": dataset_name.upper()}
 
                     run = wandb.init(project="Legal DeepAL", reinit=True, config=config, tags=[version])
-                    wandb.run.name = model_name + ' -> ALL TRAIN BASELINE'
+                    wandb.run.name = model_name + ' -> RAW BASELINE'
 
-                    _, all_train_data = dataset.get_train_data()
-                    net.train(all_train_data, train_params)
                     preds = net.predict(dataset.get_test_data()).cpu()
                     accuracy = dataset.cal_test_acc(preds)
-                    print(f"Baseline testing accuracy: {accuracy}")
+                    print(f"Raw Baseline testing accuracy: {accuracy}")
 
                     wandb.log({
-                        "total_labeled_data": len(all_train_data),
+                        "total_labeled_data": 0,
                         'predictions': preds.numpy(),
                         'test_accuracy': accuracy})
                     run.finish()
                     print("================================================================\n")
-            #############################################
+
+                    if model_name != 'SBERT_STJ_IRIS':
+                        config = {"train_config": train_params,
+                                  "model": model_name,
+                                  "test_data": test_data,
+                                  "seed": seed,
+                                  "samples": sample,
+                                  "strategy": "ALL TRAIN DATA",
+                                  "dataset": dataset_name.upper()}
+
+                        run = wandb.init(project="Legal DeepAL", reinit=True, config=config, tags=[version])
+                        wandb.run.name = model_name + ' -> ALL TRAIN BASELINE'
+
+                        _, all_train_data = dataset.get_train_data()
+                        net.train(all_train_data, train_params)
+                        preds = net.predict(dataset.get_test_data()).cpu()
+                        accuracy = dataset.cal_test_acc(preds)
+                        print(f"Baseline testing accuracy: {accuracy}")
+
+                        wandb.log({
+                            "total_labeled_data": len(all_train_data),
+                            'predictions': preds.numpy(),
+                            'test_accuracy': accuracy})
+                        run.finish()
+                        net = None
+                        print("================================================================\n")
+                #############################################
             for n_init_labeled in n_init_labeleds:
                 for n_query in n_queries:
                     for strategy_name in strategies:
                         print("============================>" + strategy_name + "<=============================\n")
                         for model_name, model_path in nsp_base_models.items():
-                            print("============================>" + model_name + "<=============================\n")
-                            config = {
-                                "n_init_labeled": n_init_labeled,
-                                "n_round": n_round,
-                                "n_query": n_query,
-                                "train_config": train_params,
-                                "model": model_name,
-                                "seed": seed,
-                                "samples": sample,
-                                "dataset": dataset_name.upper()
-                            }
+                            with ClearCache():
+                                print("============================>" + model_name + "<=============================\n")
+                                config = {
+                                    "n_init_labeled": n_init_labeled,
+                                    "n_round": n_round,
+                                    "n_query": n_query,
+                                    "train_config": train_params,
+                                    "model": model_name,
+                                    "seed": seed,
+                                    "samples": sample,
+                                    "dataset": dataset_name.upper()
+                                }
 
-                            dataset = get_STS_data(dataset_name, sample, seed)  # load dataset
-                            net = CustomNet(BertForNSP, train_params, device, model_path)
-                            test_data = np.fromiter(map(lambda x: x.label, dataset.get_test_data()), dtype=int)
+                                dataset = get_STS_data(dataset_name, sample, seed)  # load dataset
+                                net = CustomNet(BertForNSP, train_params, device, model_path)
+                                test_data = np.fromiter(map(lambda x: x.label, dataset.get_test_data()), dtype=int)
 
-                            strategy = get_strategy(strategy_name)(dataset, net)  # load strategy
+                                strategy = get_strategy(strategy_name)(dataset, net)  # load strategy
 
-                            dataset.initialize_labels(n_init_labeled)
-                            print(f"number of labeled pool: {n_init_labeled}")
-                            print(f"number of unlabeled pool: {dataset.n_pool - n_init_labeled}")
-                            print(f"number of unlabeled query data: {n_query}")
-                            print(f"number of testing pool: {dataset.n_test}")
-                            print()
+                                dataset.initialize_labels(n_init_labeled)
+                                print(f"number of labeled pool: {n_init_labeled}")
+                                print(f"number of unlabeled pool: {dataset.n_pool - n_init_labeled}")
+                                print(f"number of unlabeled query data: {n_query}")
+                                print(f"number of testing pool: {dataset.n_test}")
+                                print()
 
-                            # round 0 accuracy
-                            print("Round 0")
-                            _, initial_labeled_data = dataset.get_labeled_data()
-                            initial_labeled_data = np.fromiter(map(lambda x: x.label, initial_labeled_data), dtype=int)
-                            print(f"Initial labeled data: \t{initial_labeled_data}")
+                                # round 0 accuracy
+                                print("Round 0")
+                                _, initial_labeled_data = dataset.get_labeled_data()
+                                initial_labeled_data = np.fromiter(map(lambda x: x.label, initial_labeled_data), dtype=int)
+                                print(f"Initial labeled data: \t{initial_labeled_data}")
 
-                            config["n_initial_unlabeled_pool"] = dataset.n_pool - n_init_labeled
-                            config["initial_labeled_data"] = initial_labeled_data,
-                            config['test_data'] = test_data
-                            config['strategy'] = strategy_name
+                                config["n_initial_unlabeled_pool"] = dataset.n_pool - n_init_labeled
+                                config["initial_labeled_data"] = initial_labeled_data,
+                                config['test_data'] = test_data
+                                config['strategy'] = strategy_name
 
-                            run = wandb.init(project="Legal DeepAL", reinit=True, config=config, tags=[version])
-                            wandb.run.name = model_name + ' -> ' + strategy_name
+                                run = wandb.init(project="Legal DeepAL", reinit=True, config=config, tags=[version])
+                                wandb.run.name = model_name + ' -> ' + strategy_name
 
-                            strategy.train()
-                            preds = strategy.predict(dataset.get_test_data()).cpu()
-                            accuracy = dataset.cal_test_acc(preds)
-                            print(f"Round 0 testing accuracy: {accuracy}")
-                            print(f"Test data: \t{test_data}")
-                            print(f"Predictions: \t{preds.numpy()}")
-
-                            wandb.log({
-                                "round": 0,
-                                "new_labeled_data": initial_labeled_data,
-                                "predictions": preds.numpy(),
-                                "test_accuracy": accuracy,
-                                "total_labeled_data": len(initial_labeled_data)})
-
-                            for rd in range(1, n_round + 1):
-                                print(f"\nRound {rd}")
-
-                                # query
-                                query_idxs = strategy.query(n_query)
-                                strategy.update(query_idxs)
-                                _, new_labeled_data = dataset.get_query_data(query_idxs)
-                                _, labeled_data = dataset.get_labeled_data()
-                                new_labeled_data = np.fromiter(map(lambda x: x.label, new_labeled_data), dtype=int)
-                                print(f"New labeled data: \t{new_labeled_data}")
-                                # update labels
-                                # strategy.train()
-                                strategy.incremental_train(query_idxs)
-
-                                # calculate accuracy
+                                strategy.train()
                                 preds = strategy.predict(dataset.get_test_data()).cpu()
                                 accuracy = dataset.cal_test_acc(preds)
-
-                                print(f"Round {rd} testing accuracy: {accuracy}")
+                                print(f"Round 0 testing accuracy: {accuracy}")
                                 print(f"Test data: \t{test_data}")
                                 print(f"Predictions: \t{preds.numpy()}")
 
                                 wandb.log({
-                                    "round": rd,
-                                    "new_labeled_data": new_labeled_data,
+                                    "round": 0,
+                                    "new_labeled_data": initial_labeled_data,
                                     "predictions": preds.numpy(),
-                                    "total_labeled_data": len(labeled_data),
-                                    'test_accuracy': accuracy
-                                })
-                            run.finish()
-                        print("================================================================")
+                                    "test_accuracy": accuracy,
+                                    "total_labeled_data": len(initial_labeled_data)})
+
+                                for rd in range(1, n_round + 1):
+                                    print(f"\nRound {rd}")
+
+                                    # query
+                                    query_idxs = strategy.query(n_query)
+                                    strategy.update(query_idxs)
+                                    _, new_labeled_data = dataset.get_query_data(query_idxs)
+                                    _, labeled_data = dataset.get_labeled_data()
+                                    new_labeled_data = np.fromiter(map(lambda x: x.label, new_labeled_data), dtype=int)
+                                    print(f"New labeled data: \t{new_labeled_data}")
+                                    # update labels
+                                    # strategy.train()
+                                    strategy.incremental_train(query_idxs)
+
+                                    # calculate accuracy
+                                    preds = strategy.predict(dataset.get_test_data()).cpu()
+                                    accuracy = dataset.cal_test_acc(preds)
+
+                                    print(f"Round {rd} testing accuracy: {accuracy}")
+                                    print(f"Test data: \t{test_data}")
+                                    print(f"Predictions: \t{preds.numpy()}")
+
+                                    wandb.log({
+                                        "round": rd,
+                                        "new_labeled_data": new_labeled_data,
+                                        "predictions": preds.numpy(),
+                                        "total_labeled_data": len(labeled_data),
+                                        'test_accuracy': accuracy
+                                    })
+                                run.finish()
+                                net = None
+                                print("================================================================")
